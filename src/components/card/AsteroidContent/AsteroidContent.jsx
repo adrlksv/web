9465a91554bbcf4af7deed25f2b92802bb3670d4
adrlksv <Qwerty123Andrey@bk.ru>
2023-05-22 01:19:04 +0300

    Simple form

--- src/components/card/AsteroidContent/AsteroidContent.jsx
+++ src/components/card/AsteroidContent/AsteroidContent.jsx
@@ -1,8 +1,6 @@
 import styles from "./AsteroidContent.module.css"
 export const AsteroidContent = (props) =>{
-
     const {name, date, distance, size,distanceMode} = props;
-
     return (
         <div>
             <div className={styles.contentName}>
@@ -14,6 +12,7 @@ export const AsteroidContent = (props) =>{
                 </div>
                 <div className={styles.contentDistance}>
                     {`Расстояние: ${distanceModes(distanceMode, distance)}`}
+                    {distanceMode ? `Расстояние: ${distance.kilometers} км` : `Расстояние: ${distance.lunar} Лун`}
                 </div>
                 <div className={styles.contentSize}>
                     {`Размер: ${size} м`}
