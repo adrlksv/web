d7d5a2846fcc28368bb67d366eb13bc544adf5d5
adrlksv <Qwerty123Andrey@bk.ru>
2023-05-23 15:44:34 +0300

    task-10

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,36 @@
+import { AsteroidContent } from "./AsteroidContent/AsteroidContent";
+import { AsteroidAction } from "./AsteroidAction/AsteroidAction";
+import { AsteroidImage } from "./AsteroidImage/AsteroidImage";
+import styles from "./Card.module.css";
+
+type AsteroidCardProps = {
+    name: string;
+    date: string;
+    distance: {
+        kilometers: number;
+        lunar: number;
+    };
+    size: number;
+    isDangerous: boolean;
+    distanceMode: boolean;
+};
+
+export const AsteroidCard = (props: AsteroidCardProps) => {
+    const { name, date, distance, size, isDangerous, distanceMode } = props;
+
+    return (
+        <div className={styles.card}>
+            <div className={isDangerous ? styles.cardRed : styles.normCard}>
+                <AsteroidImage />
+                <AsteroidContent
+                    name={name}
+                    date={date}
+                    distance={distance}
+                    size={size}
+                    distanceMode={distanceMode}
+                />
+                <AsteroidAction isDangerous={isDangerous} />
+            </div>
+        </div>
+    );
+};
---
+++ src/pages/Asteroids.tsx
@@ -0,0 +1,114 @@
+import { AsteroidCard } from "../components/card/Card";
+import styles from "./Asteroids.module.css";
+import { Header } from "../components/header/Header";
+import { Link, LinkProps } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+export const Asteroids = () => {
+    const [asteroids, setAsteroids] = useState([]);
+    const [onlyDangerous, setOnlyDangerous] = useState(false);
+    const [distanceMode, setDistanceMode] = useState(true);
+
+    useEffect(() => {
+        try {
+            fetch("https://api.nasa.gov/neo/rest/v1/feed?api_key=IBHHw7GM4HWxZRxUQpSQZIylGjEwuUbIWy6IeXwX")
+                .then((res) => res.json())
+                .then((response) => {
+                    let rawAsteroids = [];
+                    for (const data in response.near_earth_objects) {
+                        rawAsteroids = rawAsteroids.concat(response.near_earth_objects[data]);
+                    }
+                    const asteroids = rawAsteroids.map((item) => {
+                        const size = Math.trunc((item.estimated_diameter.meters.estimated_diameter_max + item.estimated_diameter.meters.estimated_diameter_min) / 2);
+                        const close = item.close_approach_data[0];
+                        return {
+                            name: item.name,
+                            date: close.close_approach_date,
+                            size,
+                            distance: {
+                                kilometers: close.miss_distance.kilometers,
+                                lunar: close.miss_distance.lunar,
+                            },
+                            isDangerous: item.is_potentially_hazardous_asteroid,
+                            id: item.id,
+                        };
+                    });
+                    setAsteroids(asteroids);
+                });
+        } catch (err) {
+            console.log(err);
+            setAsteroids(generateAsteroids());
+        }
+    }, []);
+
+    const handleOnlyDangerousChange = () => {
+        setOnlyDangerous(!onlyDangerous);
+    };
+
+    const handleDistanceModeChange = (mode) => {
+        setDistanceMode(mode);
+    };
+
+    return (
+        <div>
+            <div>
+                <Header />
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <input type="checkbox" checked={onlyDangerous} onChange={handleOnlyDangerousChange} />
+                        <label>Показать только опасные</label>
+                    </div>
+                    <div className={styles.distances}>
+                        <label>Расстояние</label>
+                        <Link to="/" onClick={() => setDistanceMode(true)}>в километрах</Link>
+                        <Link to="/" onClick={() => setDistanceMode(false)}>в дистанциях до луны</Link>
+                    </div>
+                </div>
+                <div>
+                    {onlyDangerous
+                        ? asteroids
+                            .filter((item) => item.isDangerous)
+                            .map((item) => <AsteroidCard key={item.id} {...item} distanceMode={distanceMode} />)
+                        : asteroids.map((item) => <AsteroidCard key={item.id} {...item} distanceMode={distanceMode} />)}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const generateAsteroids = () => {
+    const months = [
+        `января`,
+        `февраля`,
+        `марта`,
+        `апреля`,
+        `мая`,
+        `июня`,
+        `июля`,
+        `августа`,
+        `сентября`,
+        `октября`,
+        `ноября`,
+        `декабря`,
+    ];
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const result = [];
+    for (let i = 0; i < parseInt((Math.random() * 20 + 2).toFixed(0)); i++) {
+        let namee = "";
+        for (let j = 0; j < parseInt((Math.random() * 10 + 2).toFixed(0)); j++) {
+            namee = "";
+            for (let j = 0; j < 4; j++) {
+                namee += characters[(Math.random() * 25).toFixed(0)];
+            }
+            const name = namee;
+            const date = `${(Math.random() * 27 + 1).toFixed(0)} ${months[(Math.random() * 12).toFixed(0)]} 2023`;
+            const size = (Math.random() * 100 + 10).toFixed(0);
+            const distance = (Math.random() * 90000000).toFixed(0);
+            const isDangerous = Math.random() >= 0.5;
+            result.push({ name, date, size, distance, isDangerous, id: name });
+        }
+    }
+    return result;
+};
